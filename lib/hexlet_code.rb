b2552bc6707435b346e8f0a657a436fa21095acd
aidys23 <aidongak23@gmail.com>
2021-10-21 14:01:24 +0700

    generate tags

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+
+  # create a html element
+  module Tag
+    def build(tag_name, **hash)
+      attributes = hash.each_with_object(+'') do |(key, value), acc|
+        acc << " #{key}=\"#{value}\""
+      end
+      if block_given?
+        "<#{tag_name}#{attributes}>#{yield}</#{tag_name}>"
+      else
+        "<#{tag_name}#{attributes}>"
+      end
+    end
+    module_function :build
+  end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -1,13 +1,28 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
+require 'hexlet_code'
 
 class HexletCodeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_method_that_creates_void_elements
+    void_element = HexletCode::Tag.build('br')
+    void_element_with_attribute = HexletCode::Tag.build('img', src: 'path/to/image')
+    void_element_with_attributes = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+
+    assert(void_element == '<br>')
+    assert(void_element_with_attribute == '<img src="path/to/image">')
+    assert(void_element_with_attributes == '<input type="submit" value="Save">')
+  end
+
+  def test_method_that_creates_element_with_closing_tag
+    element = HexletCode::Tag.build('label') { 'Email' }
+    element_with_attribute = HexletCode::Tag.build('label', for: 'email') { 'Email' }
+
+    assert(element == '<label>Email</label>')
+    assert(element_with_attribute == '<label for="email">Email</label>')
   end
 end
