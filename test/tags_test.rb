c73dbd78501507eeca1d9b3c3cc0dab84b43e80d
aidys23 <aidongak23@gmail.com>
2021-10-27 09:56:09 +0700

    fix tag test

--- test/tags_test.rb
+++ test/tags_test.rb
@@ -6,16 +6,16 @@ class TagsTest < Minitest::Test
     void_element_with_attribute = HexletCode::Tag.build('img', src: 'path/to/image')
     void_element_with_attributes = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
 
-    assert_equal(void_element, '<br>')
-    assert_equal(void_element_with_attribute, '<img src="path/to/image">')
-    assert_equal(void_element_with_attributes, '<input type="submit" value="Save">')
+    assert_equal('<br>', void_element)
+    assert_equal('<img src="path/to/image">', void_element_with_attribute)
+    assert_equal('<input type="submit" value="Save">', void_element_with_attributes)
   end
 
   def test_method_that_creates_element_with_closing_tag
     element = HexletCode::Tag.build('label') { 'Email' }
     element_with_attribute = HexletCode::Tag.build('label', for: 'email') { 'Email' }
 
-    assert_equal(element, '<label>Email</label>')
-    assert_equal(element_with_attribute, '<label for="email">Email</label>')
+    assert_equal('<label>Email</label>', element)
+    assert_equal('<label for="email">Email</label>', element_with_attribute)
   end
 end
