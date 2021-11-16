609a25808fe53d4092b7a9b32f50da5491b22890
aidys23 <aidongak23@gmail.com>
2021-11-16 12:35:20 +0700

    transform string fixtures to html files

--- test/fixtures/html_tags.rb
+++ test/fixtures/html_tags.rb
@@ -2,30 +2,11 @@
 
 module Fixture
   FORMS = {
-    form: '<form action="#" method="post">' \
-          '<label for="name">Name</label>' \
-          '<input name="name" type="text" value="rob">' \
-          '</form>',
-    form_with_action: '<form action="/users" method="post">' \
-                      '<label for="name">Name</label>' \
-                      '<input name="name" type="text" value="rob">' \
-                      '</form>',
-    form_with_input: '<form action="#" method="post">'\
-                     '<label for="name">Name</label>' \
-                     '<input class="user-name" name="name" type="text" value="rob">'\
-                     '</form>',
-    form_with_textarea: '<form action="#" method="post">' \
-                        '<label for="job">Job</label>' \
-                        '<textarea name="job" cols="20" rows="40">hexlet</textarea>' \
-                        '</form>',
-    form_textarea_with_options: '<form action="#" method="post">' \
-                                '<label for="job">Job</label>' \
-                                '<textarea name="job" cols="50" rows="50">hexlet</textarea>' \
-                                '</form>',
-    form_with_submit_button: '<form action="#" method="post">' \
-                             '<label for="name">Name</label>' \
-                             '<input name="name" type="text" value="rob">'\
-                             '<input name="commit" type="submit" value="Send">'\
-                             '</form>'
+    form: File.read("#{__dir__}/form.html").delete("\n"),
+    form_with_action: File.read("#{__dir__}/form_with_action.html").delete("\n"),
+    form_with_input: File.read("#{__dir__}/form_with_input.html").delete("\n"),
+    form_with_textarea: File.read("#{__dir__}/form_with_textarea.html").delete("\n"),
+    form_textarea_with_options: File.read("#{__dir__}/form_textarea_with_option.html").delete("\n"),
+    form_with_submit_button: File.read("#{__dir__}/form_with_submit_button.html").delete("\n")
   }.freeze
 end
