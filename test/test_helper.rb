d8a97ffd836fdc2c80be65c35827d1481a108cf6
aidys23 <aidongak23@gmail.com>
2021-11-18 09:50:32 +0700

    delete excess requirement

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'hexlet_code'
 require_relative 'fixtures/user'
-require_relative 'fixtures/html_tags'
 
 require 'minitest/autorun'
 
